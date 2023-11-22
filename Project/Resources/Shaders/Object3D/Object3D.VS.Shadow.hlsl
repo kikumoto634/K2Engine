38b1062947d79a43456a2c36152fc3ca6e7048f2
GMHK_SAN（KIKUMOTO） <74335323+kikumoto634@users.noreply.github.com>
2023-11-22 16:46:03 +0900

    Shadowシェーダーで使用する構造体たちが抵当な箇所で使用できるようにルートシグネチャを変更しよう

---
+++ Project/Resources/Shaders/Object3D/Object3D.VS.Shadow.hlsl
@@ -0,0 +1,9 @@
+#include "Object3D.hlsli"
+
+float4 main(/*VertexShaderInput input*/) : SV_POSITION
+{
+    //float4 pos = float4(input.position.xyz,1.0f);
+    //pos = mul(pos, gTransformationMatrix.World);
+    //pos = mul(pos, gDirectionalLight.);
+    return (1, 1, 1, 1);
+}
