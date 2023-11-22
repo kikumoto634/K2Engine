bf3cb787db754c3c9d7fc5554980e148a7d5992a
GMHK_SAN（KIKUMOTO） <74335323+kikumoto634@users.noreply.github.com>
2023-11-22 16:55:11 +0900

    ToDo記載

--- Project/Resources/Shaders/Object3D/Object3D.VS.Shadow.hlsl
+++ Project/Resources/Shaders/Object3D/Object3D.VS.Shadow.hlsl
@@ -7,3 +7,13 @@ float4 main(/*VertexShaderInput input*/) : SV_POSITION
     //pos = mul(pos, gDirectionalLight.);
     return (1, 1, 1, 1);
 }
+
+//モデルの行列とライトの行列が必要
+//二つともGeometryBaseのRootSignatureで設定しているので
+//ShadowMap専用のCommonとPipelineを作成するクラスの作成
+//そこで必要な情報をシェーダーに送れるようにする
+//それと同時にLightクラスにて、Directionalを改造、ライトから見たときの行列の作成
+//VPなので、カメラなどでどのように作成されているのか
+//座標、回転値を逆数に掛けて、View,
+//平行投影でprojection
+//両方を掛けてVP、それを代入しよう
